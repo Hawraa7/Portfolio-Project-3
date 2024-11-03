f844fb8e7d4b15d2982a4d86ff6e6af5d5881a0c
Hawraa Hijazi <hawraahijazi1996@gmail.com>
2024-11-03 13:57:10 +0000

    Initial commit

--- run.py
+++ run.py
@@ -4,7 +4,18 @@ import json
 
 with open('creds.json', 'r') as file:
     JsonFile = json.load(file)
-
 APIkey = JsonFile['API_KEY']
-print(APIkey)
+
+
+
+
+
+symbol = "AAPL"
+#url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={APIkey}"
+url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={APIkey}"
+response = requests.get(url)
+stock_data = response.json()
+#print(stock_data['Time Series (Daily)']['2024-11-01']['2. high'])
+print(stock_data["Global Quote"]["05. price"])
+
 
