1e3df1dc4106c1a8dd43f7085580ebefc844d3d1
Hawraa Hijazi <hawraahijazi1996@gmail.com>
2024-11-11 21:20:46 +0000

    update the main function so that it is more readable. Also we fetch now the live prices of stocks (get_stock_price function) using the yahoo finance API. Third the list of stock symbols are now loaded directly from an offline saved file

--- run.py
+++ run.py
@@ -1,13 +1,25 @@
 import requests
 import json
 import csv
+import yfinance as yf
+import os
+
 """ Including the Alpha Vantage API key """
 
 with open('creds.json', 'r') as file:
     JsonFile = json.load(file)
 APIkey = JsonFile['API_KEY']
 
-def get_stock_price(symbol):
+def clear_terminal():
+    # Check the operating system and run the appropriate clear command
+    if os.name == 'nt':  # For Windows
+        os.system('cls')
+    else:  # For Mac and Linux
+        os.system('clear')
+
+
+
+def get_stock_price_old(symbol):
     """  
     Retrieves the current stock price for a given symbol using the Alpha Vantage API,
     Uses the "GLOBAL_QUOTE" function to obtain real-time quote data,
@@ -19,13 +31,23 @@ def get_stock_price(symbol):
     stock_price = float(stock_data["Global Quote"]["05. price"])
     return stock_price
 
-def get_symbol_list():
+def get_stock_price(symbol):
+    """  
+    Retrieves the current stock price for a given symbol using the Alpha Vantage API,
+    Uses the "GLOBAL_QUOTE" function to obtain real-time quote data,
+    Parses the JSON response and extracts the stock price, converting it to a float for accuracy.
+    """
+    stock = yf.Ticker(symbol)
+    stock_price = stock.history(period="1d")['Close'].iloc[0]
+    return stock_price
+
+def get_symbol_list_old():
     """ 
     Retrieves a list of stock symbols for all US-listed companies from the Alpha Vantage API,
     Reads the response content as CSV data and extracts each stock symbol,
     Skips the header row in the CSV data to start the list from the first symbol.
-    """
     url = f"https://www.alphavantage.co/query?function=LISTING_STATUS&apikey={APIkey}"
+    """
     response = requests.get(url)
     if response.status_code == 200:
         content = response.content.decode('utf-8').splitlines()
@@ -35,9 +57,19 @@ def get_symbol_list():
         print(f"Cannot retrieve the data: {response.status_code}")
     return symbol_list
 
+def get_symbol_list():
+    """ 
+    Retrieves a list of stock symbols for all US-listed companies from the Alpha Vantage API,
+    Reads the response content as CSV data and extracts each stock symbol,
+    Skips the header row in the CSV data to start the list from the first symbol.
+    """
+    with open('stock_list.txt', 'r') as file:
+        symbol_list = json.load(file)
+    return symbol_list
+
 class Portfolio:
     def __init__(self, investment):
-        """ Initializes a Portfolio object with a given investment amount """
+        """ Initializes a Portfolio object with a given investment amount """ 
         self.stock = {}
         self.investment = investment
         self.account_value = investment
@@ -58,6 +90,7 @@ class Portfolio:
             else:
                 self.stock[symbol] = number
             self.buying_power -= overall_price
+            print(f"You have successfully added {number} {symbol} to your portfolio.")
         else:
             print("You do not have enough buying power!") 
     
@@ -74,8 +107,9 @@ class Portfolio:
                 overall_price = stock_price * number
                 self.buying_power += overall_price
                 self.stock[symbol] -= number
+                print(f"You have successfully sold {number} {symbol} from your portfolio.")
             else: 
-                print(f"You do not have enough number of {symbol} stocks to sell")
+                print(f"You do not have enough number of '{symbol}' stocks to sell")
         else:
             print(f"The stock symbol '{symbol}' is not in the portfolio.")
         
@@ -93,6 +127,7 @@ class Portfolio:
         """ Increases the portfolio's total investment and buying power by a specified amount."""
         self.investment += amount
         self.buying_power += amount 
+        print(f"You have successfully added {number} to your account.")
     
     def withdraw(self, amount):
         """
@@ -102,6 +137,7 @@ class Portfolio:
         if self.buying_power >= amount:
             self.investment -= amount
             self.buying_power -= amount 
+            print(f"You have successfully withdrawn {number} from your account.")
         else:
             print("You do not have enough liquidity!")
 
@@ -110,16 +146,28 @@ class Portfolio:
         Prints the current status of the portfolio, including buying power, account value, total investment, and stock holdings.
         """
         self.update_account_value()
-        print(f"The buying power is: {self.buying_power}, the account value is {self.account_value}, the investment is {self.investment}, and the stocks are {self.stock}")
+        print(f"Your buying power is: {self.buying_power}, your account value is {self.account_value}, your investment is {self.investment}, and the stocks in your portfolio are {self.stock}")
 
 def main():
     """ Run all program functions """ 
     selection = 100
+    symbol_list = get_symbol_list()
     initial_investment = float(input("Welcome to Hawraa Trading Platform. Enter the amount you want to invest in your portfolio: \n"))
     my_portfolio = Portfolio(initial_investment)
+    print("Congratulations!! You have successfully created your portfolio!!")
+
     while selection > 0:
-        symbol_list = get_symbol_list()
-        selection = int(input("Please select 1 to buy a stock, 2 to sell a stock, 3 to increase your investment, 4 to withdraw from your account, 5 to check your account status or 0 to quit: \n"))
+        input("Press any key to continue...")
+        clear_terminal()
+        my_portfolio.print_status()
+        print("Which operation would you like to do? Please choose an option by entering the corresponding number:")
+        print("1 - Buy a stock")
+        print("2 - Sell a stock")
+        print("3 - Increase your investment")
+        print("4 - Withdraw from your account")
+        print("0 - Quit")
+
+        selection = int(input("\n"))
         match selection:
             case 1:
                 symbol = input("Enter the stock name: \n")
@@ -169,12 +217,10 @@ def main():
                         print("The number you entered needs to be greater than zero!")
                 except:
                     print("The value you entered is invalid!")
-            case 5:
-                my_portfolio.print_status()
             case 0:
-                print("Thanks!")
+                print("Thanks for using our platform!")
             case _:
-                print("Please select 1, 2, 3, 4, 5, or 0")
+                print("Please select 1, 2, 3, 4, or 0")
             
                 
 
